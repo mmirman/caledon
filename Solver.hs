f149bc31e9af427f0cb457de2ed290d3f7debade
Matthew Mirman <titaniumeyes@aol.com>
2012-12-08 23:05:21 -0500

    added Implicits

--- Solver.hs
+++ Solver.hs
@@ -22,9 +22,136 @@ import Control.Monad.Trans.Class
 import Data.Monoid
 import Data.Functor
 import Data.Traversable (forM)
-import Data.Foldable as F (msum, forM_, fold, Foldable, foldl')
+import Data.Foldable as F (msum, forM_, foldr', foldl', fold, Foldable, foldl')
 import qualified Data.Map as M
 import qualified Data.Set as S
+
+
+--------------------------------------------------------------------
+----------------------- UNIFICATION --------------------------------
+--------------------------------------------------------------------
+type VarGen = StateT Integer Choice
+
+
+class HasVar a where
+  getV :: a -> Integer
+  setV :: Integer -> a -> a
+instance HasVar Integer where  
+  getV = id
+  setV a _ = a
+      
+unifyAll envE env [] = return mempty
+unifyAll envE env (a:l) = do
+  s <- appendErr ("IN: "++show a) $ unify envE env a
+  s' <- unifyAll envE env (subst s l)
+  return $ s *** s'
+
+unify :: M.Map Name Tp -> M.Map Name Tp -> Constraint Tm -> VarGen Substitution
+unify envE env constraint@(a :=: b) = 
+  let badConstraint = throwError $ show constraint ++" \nWITH: "++show (M.toList env)
+      unify'' = unify envE env
+      unify' = unify envE
+      doUntoBoth m n = F.foldl' act (return mempty) (zip m n)
+      act prev (mt,nt) = do
+        sub <- prev 
+        sub' <- unify' (subst sub env) $ subst sub (tpToTm mt :=: tpToTm nt)
+        return $ sub *** sub'
+
+  in case a :=: b of
+    _ :=: _ | a > b -> unify' env $ b :=: a
+    AbsImp n ty t :=: _ -> do
+      undefined
+      (v',s) <- solve $ (M.toList envE)++(M.toList env) :|- ty
+      unify' env $ rebuildSpine (subst s $ Abs n ty t) [Atom $ v'] :=: (subst s b)
+    Abs n ty t :=: Abs n' ty' t' -> do  
+      s <- unify'' $ tpToTm ty :=: tpToTm ty'
+      nm <- getNew
+      s' <- unify' (subst s $ M.insert nm ty env) $ subst (M.insert n (var nm) s) t :=: subst (M.insert n' (var nm) s) t'
+      return $ s *** M.delete nm s'
+    Abs n ty t :=: _ -> do  
+      nm <- getNew
+      s <- unify' (M.insert nm ty env) $ subst (n |-> var nm) t :=: rebuildSpine b [Atom $ var nm]
+      return $ M.delete nm s
+    
+    Spine (Var a) [] :=: Spine (Var a') [] | a == a' -> 
+      return mempty
+    Spine (Var a) [] :=: _ | not (M.member a env) && (not $ S.member a $ freeVariables b) -> 
+      return $ a |-> b
+    Spine (Var a) m :=: Spine (Var a') n | a == a' && M.member a env && length m == length n ->  -- the var has the same rule whether it is quantified or not.
+      doUntoBoth m n
+      
+    Spine (Cons c) _ :=: Spine (Cons c') _ | c /= c' -> 
+      badConstraint
+    Spine (Cons c) m :=: Spine (Cons c') n | length m == length n -> 
+      doUntoBoth m n
+      
+    Spine (Var x) n :=: Spine (Cons c) m | not $ M.member x env -> do -- Gvar-Const
+      us <- forM n $ const $ getNew
+      usty <- forM n $ const $ Atom <$> var <$> getNew
+      xs <- forM m $ const $ Var <$> getNew
+      let l = Spine (Cons c) $ (\xi -> Atom $ Spine xi $ Atom <$> var <$> us) <$> xs
+          s = x |-> foldr' (\(v,ty) t -> Abs v ty t) l (zip us usty)
+      s' <- unify' (subst s env) $ subst s (a :=: b)
+      return $ (s *** s')
+      
+    Spine (Var a) n :=: Spine (Var a') m | a == a' && length m == length n -> do -- Gvar-Gvar - same
+      h <- getNew
+      let act (xi, yi) next = do
+            vi <- getNew
+            sub' <- catchError (Just <$> unify'' (tpToTm xi :=: tpToTm yi)) $ \_ -> return Nothing
+            (xs,zs,sub) <- next
+            case sub' of
+              Just sub' -> return (vi:xs,vi:zs, sub' *** sub)              
+              Nothing   -> return (vi:xs,zs, sub)
+                
+      (xs,zs,sub) <- foldr' act (return (mempty, mempty, mempty)) (zip n m)
+      
+      let base = Spine (Var h) (Atom <$> var <$> zs)
+          f' = foldr' (\v t -> Abs v undefined t) base xs
+      return (sub *** a |-> f')
+      
+    Spine (Var f) xs :=: Spine (Var g) ys | f /= g -> do -- Gvar-Gvar - diff
+      h <- getNew
+      let act xi next = do
+            sub' <- flip catchError (const $ return Nothing) $ msum $ flip fmap ys $ \yi -> do
+              sub <- unify'' (tpToTm xi :=: tpToTm yi)
+              return $ Just (yi,sub)
+            (zs,sub) <- next
+            case sub' of
+              Just (yi,sub') -> return ((xi,yi):zs, sub' *** sub)              
+              Nothing  -> return (zs, sub)
+      (zs,sub) <- foldr' act (return (mempty, mempty)) xs
+      
+      let toVar (Atom (Spine (Var x) [])) = do
+            t <- getNew
+            return (x, Atom $ var t)
+          toVar _ = badConstraint
+      
+      xs' <- mapM toVar xs
+      ys' <- mapM toVar ys
+      
+      let baseF = Spine (Var h) (fst <$> zs)
+          f' = foldr' (\(v,vt) t -> Abs v vt t) baseF xs'
+          
+          baseG = Spine (Var h) (snd <$> zs)
+          g' = foldr' (\(v,vt) t -> Abs v vt t) baseG ys'
+      return (sub *** (f |-> f' *** g |-> g'))
+      
+    _ :=: _ -> badConstraint
+
+genUnifyEngine env consts = do
+  s <- unifyAll env mempty consts
+  return $ finishSubst s
+  
+recSubst :: (Eq b, Subst b) => Substitution -> b -> b
+recSubst s f = fst $ head $ dropWhile (not . uncurry (==)) $ iterate (\(_,b) -> (b,subst s b)) (f,subst s f)  
+
+finishSubst s = recSubst s s
+
+finishSubstWith w s = case M.lookup w s of
+  Just (Spine (Var v) []) -> finishSubst $ M.insert v (var w) (M.delete w s)
+  _ -> s
+
 
 ----------------------------------------------------------------------
 ----------------------- LOGIC ENGINE ---------------------------------
@@ -41,7 +168,7 @@ unifier cons t = do
   let isAtom (Atom _) = True
       isAtom _ = False
   msum $ flip map (filter (isAtom . snd) cons) $ \(x,Atom con) -> do
-    s <- genUnifyEngine [con :=: t']
+    s <- genUnifyEngine (M.fromList cons) [con :=: t']
     return $ (var x,s) 
 
 left :: Judgement -> Reification
@@ -77,7 +204,7 @@ natural :: [(Name, Tp)] -> (Tm,Tp) -> Deduction
 natural cont (tm,ty) = do
   let env = M.fromList cont
   (_, constraints) <- runWriterT $ checkTerm env tm ty
-  genUnifyEngine constraints
+  genUnifyEngine (M.fromList cont) constraints
   
 
 solver :: [(Name,Tp)] -> Tp -> Either String [(Name, Tm)]
@@ -134,7 +261,7 @@ checkTerm env v t = case v of
     v2  <- (++':':'>':nm) <$> getNew
     tell [ tpToTm (ForallImp v1 ty $ Atom $ var v2) :=: tpToTm t ]
     ((tm',t'),constraints) <- listen $ checkTerm (M.insert nm' ty env) (subst (nm |-> var nm') tm) $ Atom $ var v2
-    s <- finishSubstWith nm' <$> (lift $ genUnifyEngine constraints)
+    s <- finishSubstWith nm' <$> (lift $ genUnifyEngine env constraints)
     tell $ map (\(s,t) -> var s :=: t ) $ M.toList s
     let s' = s *** nm' |-> var nm 
     return $ (AbsImp nm ty' $ subst s' tm' , ForallImp v1 ty t')
@@ -148,7 +275,7 @@ checkTerm env v t = case v of
     tell [ tpToTm (Forall v1 ty $ Atom $ var v2) :=: tpToTm t ]
     
     ((tm',t'),constraints) <- listen $ checkTerm (M.insert nm' ty env) (subst (nm |-> var nm') tm) $ Atom $ var v2
-    s <- finishSubstWith nm' <$> (lift $ genUnifyEngine constraints)
+    s <- finishSubstWith nm' <$> (lift $ genUnifyEngine env constraints)
     tell $ map (\(s,t) -> var s :=: t ) $ M.toList s
     let s' = s *** nm' |-> var nm 
     return $ (Abs nm ty' $ subst s' tm' , Forall v1 ty t')
@@ -165,7 +292,7 @@ checkTipe env v = case v of
     ty' <- checkTipe env ty
     nm' <- (++'*':nm) <$> getNew
     (a,constraints) <- listen $ checkTipe (M.insert nm' ty env) $ subst (nm |-> var nm') t
-    s <- finishSubstWith nm' <$> (lift $ genUnifyEngine constraints)
+    s <- finishSubstWith nm' <$> (lift $ genUnifyEngine env constraints)
     
     tell $ map (\(s,t) -> var s :=: t ) $ M.toList s
     let s' = s *** nm' |-> var nm 
@@ -190,7 +317,7 @@ checkType env base ty = fmap fst $ flip runStateT 0 $ appendErr ("FOR: "++show t
     $ throwError $ "non local name \""++con++"\" expecting "++base
   (ty',constraints) <- runWriterT $ checkTipe env ty
   
-  s <- appendErr ("CONSTRAINTS: "++show constraints) $ genUnifyEngine constraints
+  s <- appendErr ("CONSTRAINTS: "++show constraints) $ genUnifyEngine env constraints
   
   return $ subst s ty'
 
